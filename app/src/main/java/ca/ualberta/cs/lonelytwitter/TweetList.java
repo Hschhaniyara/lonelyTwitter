fd707156cbd73e5a1cbea92bd23c583fb0429082
Hschhaniyara <chhaniya@ualberta.ca>
2018-10-30 18:10:30 -0600

    finished lab

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -3,25 +3,30 @@ package ca.ualberta.cs.lonelytwitter;
 import java.util.ArrayList;
 
 public class TweetList {
+    // fixed declaration for tweets
     private ArrayList<Tweet> Tweets = new ArrayList<Tweet>();
 
     public TweetList() {
 
     }
-
+    // fixed implementaion for getTweet
     public Tweet getTweet(int index){
+
         return Tweets.get(index);
     }
-
+    // fixed implementaion for hasTweet
     public boolean hasTweet(Tweet Tweet){
+
         return Tweets.contains(Tweet);
     }
-
+    // fixed implementaion for add
     public void add(Tweet Tweet) {
+
         Tweets.add(Tweet);
     }
-
+    // fixed implementaion for delete
     public void delete(Tweet Tweet) {
+
         Tweets.remove(Tweet);
     }
 }
